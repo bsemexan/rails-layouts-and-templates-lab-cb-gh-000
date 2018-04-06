3cfef9c41be15880a8dad0cbd2c8eb46ac7b896a
Berry  Semexan <bsemexan@gmail.com>
2018-04-06 00:43:17 +0000

    automatically backed up by learn

---
+++ app/controllers/store_admins_controller.rb
@@ -0,0 +1,2 @@
+class StoreAdminsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :store_admins
   resources :statics
   get 'home', to: 'static#home'
   get 'admin/home', to: 'store_admin#home'
